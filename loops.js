3bd1b733e17b7fe7f49c601180278e4d612197ac
Akhila-web <akhilareddycholleti@gmail.com>
2024-10-28 00:04:13 -0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,39 @@
+switch("saturday"){
+    case "monday":
+        console.log("lord shiva")
+        break;
+        
+    case "tuesday":
+        console.log("jai hanuman")
+        break;
+    case "wednesday":
+        console.log("sharanam ayyappa")
+        break;
+    case "thursday":
+        console.log("om sai ram")
+        break;
+       
+    case "friday":
+        console.log("jai bhavani")
+        break;
+    case "saturday":
+        console.log("govindha govindhaaa")
+        break;
+    case "sunday":
+        console.log("mallanna")
+        break;
+    default:
+        console.log("no god nothing")
+
+}
+//for... of
+var a=[1,2,3,4,5];
+for(var i=1;i<=a.length;i++){
+    console.log(i)
+}
+//for.....of 
+var a="Akhila"
+for(var char of a){
+    console.log(char)
+}
+
